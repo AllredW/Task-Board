6215c5e94738b1e170e0ea5524cee4456c8b7bb4
AllredW <allrewil@gmail.com>
2024-08-19 12:43:08 -0400

    working on creating a card for new tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,78 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const projectFormEl = $("#formModal");
 
-// Todo: create a function to generate a unique task id
+// import input values from modal
+let title = $("#taskTitle");
+let dueDate = $("#taskDue");
+let description = $("#taskbody");
+
+//create new object from input data
+const newTask = {
+  taskTitle: title.val().trim(),
+  taskDue: dueDate.val(),
+  taskBody: description.val().trim(),
+  taskID: generateTaskId(),
+  status: "to-do",
+};
+
+// Generate a unique task id
 function generateTaskId() {
+  const taskID = crypto.randomUUID();
 
+  nextId = taskID;
+
+  return nextId;
 }
 
-// Todo: create a function to create a task card
+// Create a new task card
 function createTaskCard(task) {
+  const taskCard = $("<div>")
+    .addClass("card task-card draggable my-3")
+    .attr("data-task-id", task.taskID);
+  const cardHeader = $("<div>").addClass("card-header h4").text("taskTitle");
+  const cardBody = $('<div>').addClass('card-body');
+  const cardDescription = $('<p>').addClass('card-text').text(task.taskBody);
+  const cardDueDate = $('<p>').addClass('card-text').text(task.taskDue);
+  const cardDeleteBtn = $('<button>')
+    .addClass('btn btn-danger delete')
+    .text('Delete')
+    .attr('data-task-id', task.taskID).attr('id', task.taskID);
+  cardDeleteBtn.on('click', handleDeleteTask);
 
+   // Sets the card background color based on due date.
+   if (task.taskDue && task.status !== 'done') {
+    const now = dayjs();
+    const DueDate = dayjs(task.taskDue, 'DD/MM/YYYY');
+
+    //If the task is due today, make the card yellow. If it is overdue, make it red.
+    if (now.isSame(DueDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(DueDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
+    }
+  }
+    // append task properties to card
+    cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+    taskCard.append(cardHeader, cardBody);
+  
+    // Return the card.
+    return taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
-}
+function renderTaskList() {}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
-}
+function handleAddTask(event) {}
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
+function handleDeleteTask(event) {}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
+function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
-
-});
+$(document).ready(function () {});
